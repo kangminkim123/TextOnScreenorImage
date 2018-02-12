b37838dbd8b4f7b436e5c021bde2bab54e99d0be
kangmin <kangmin.kim@ocsbstudent.ca>
2018-02-12 14:16:53 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- Hello, World!
+--
+-- created by : kangmin
+-- created on : Feb 12 2018
+-----------------------------------------------------------------------------------------
+
+local myText = display.newText( "Hello World!", 1024, 468, native.systemFont, 146 )
+myText:setFillColor( 1, 0, 0 )
+
+local image = display.newImageRect( "re.png", 1024, 2038)
