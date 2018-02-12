e164aa7d33b8ddd89600a1a05c35a4f7bf0a384c
kangmin <kangmin.kim@ocsbstudent.ca>
2018-02-12 14:42:59 -0500

    final code

--- main.lua
+++ main.lua
@@ -6,7 +6,10 @@
 -- created on : Feb 12 2018
 -----------------------------------------------------------------------------------------
 
-local myText = display.newText( "Hello World!", 1024, 468, native.systemFont, 146 )
+display.setDefault( "background", 0, 0, 1 )
+local myText = display.newText( "Hello World!", 1024, 368, native.systemFont, 146 )
 myText:setFillColor( 1, 0, 0 )
 
-local image = display.newImageRect( "re.png", 1024, 2038)
+local image = display.newImageRect( "./assets/sprites/re.png", 800, 800)
+image.x = 1024
+image.y = 700
